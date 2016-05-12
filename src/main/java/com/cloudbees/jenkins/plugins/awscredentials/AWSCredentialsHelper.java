f197d9c0e67da4d93053224d04152ca1f9a63f92
Nicolas De Loof <nicolas.deloof@gmail.com>
2016-05-12 16:54:01 +0200

    avoid code duplication in various AWS plugins

---
+++ src/main/java/com/cloudbees/jenkins/plugins/awscredentials/AWSCredentialsHelper.java
@@ -0,0 +1,43 @@
+package com.cloudbees.jenkins.plugins.awscredentials;
+
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import hudson.model.ItemGroup;
+import hudson.security.ACL;
+import hudson.util.ListBoxModel;
+import org.apache.commons.lang.StringUtils;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nullable;
+import java.util.Collections;
+
+/**
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class AWSCredentialsHelper {
+
+    private AWSCredentialsHelper() {}
+
+    @CheckForNull
+    public static AmazonWebServicesCredentials getCredentials(@Nullable String credentialsId, ItemGroup context) {
+        if (StringUtils.isBlank(credentialsId)) {
+            return null;
+        }
+        return (AmazonWebServicesCredentials) CredentialsMatchers.firstOrNull(
+                CredentialsProvider.lookupCredentials(AmazonWebServicesCredentials.class, context,
+                        ACL.SYSTEM, Collections.EMPTY_LIST),
+                CredentialsMatchers.withId(credentialsId));
+    }
+
+    public static ListBoxModel doFillCredentialsIdItems(ItemGroup context) {
+        return new StandardListBoxModel()
+                .withEmptySelection()
+                .withMatching(
+                        CredentialsMatchers.always(),
+                        CredentialsProvider.lookupCredentials(AmazonWebServicesCredentials.class,
+                                context,
+                                ACL.SYSTEM,
+                                Collections.EMPTY_LIST));
+    }
+}
