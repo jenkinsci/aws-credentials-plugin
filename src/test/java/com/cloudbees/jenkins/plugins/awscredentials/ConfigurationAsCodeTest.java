92108167ef0d3a399d7c1c9befbbc57271784e8b
David Schott <schottdb@gmail.com>
2020-06-09 11:59:25 -0400

    replace assume with assert, hehe

--- src/test/java/com/cloudbees/jenkins/plugins/awscredentials/ConfigurationAsCodeTest.java
+++ src/test/java/com/cloudbees/jenkins/plugins/awscredentials/ConfigurationAsCodeTest.java
@@ -8,9 +8,8 @@ import hudson.security.ACL;
 import io.jenkins.plugins.casc.misc.RoundTripAbstractTest;
 import org.jvnet.hudson.test.RestartableJenkinsRule;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assume.assumeThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ConfigurationAsCodeTest extends RoundTripAbstractTest {
 
@@ -20,21 +19,14 @@ public class ConfigurationAsCodeTest extends RoundTripAbstractTest {
                 CredentialsProvider.lookupCredentials(AWSCredentialsImpl.class,
                         r.j.jenkins, ACL.SYSTEM, (DomainRequirement) null),
                 CredentialsMatchers.withId("aws-credentials-casc"));
-        assumeThat("AWS credentials are not null", credentials, notNullValue());
-        assumeThat("access key is configured", credentials.getAccessKey(),
-                equalTo("foo"));
-        assumeThat("description is configured", credentials.getDescription(),
-                equalTo("foo-description"));
-        assumeThat("mfa serial is configured", credentials.getIamMfaSerialNumber(),
-                equalTo("arn:aws:iam::123456789012:mfa/user"));
-        assumeThat("role arn is configured", credentials.getIamRoleArn(),
-                equalTo("arn:aws:iam::123456789012:role/MyIAMRoleName"));
-        assumeThat("id is configured", credentials.getId(),
-                equalTo("aws-credentials-casc"));
-        assumeThat("scope is configured", credentials.getScope(),
-                equalTo(CredentialsScope.GLOBAL));
-        assumeThat("secret key is configured", credentials.getSecretKey().getPlainText(),
-                equalTo("bar"));
+        assertNotNull(credentials);
+        assertEquals(credentials.getAccessKey(), "foo");
+        assertEquals(credentials.getDescription(), "foo-description");
+        assertEquals(credentials.getIamMfaSerialNumber(), "arn:aws:iam::123456789012:mfa/user");
+        assertEquals(credentials.getIamRoleArn(), "arn:aws:iam::123456789012:role/MyIAMRoleName");
+        assertEquals(credentials.getId(), "aws-credentials-casc");
+        assertEquals(credentials.getScope(), CredentialsScope.GLOBAL);
+        assertEquals(credentials.getSecretKey().getPlainText(), "bar");
     }
 
     @Override
